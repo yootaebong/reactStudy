d99445e79b51823341bb0284cc0977ed547a4d7e
youtaebong <ytb4748@gmail.com>
2020-02-01 18:05:16 +0900

    immer project end

--- immer-tutorial/src/App.js
+++ immer-tutorial/src/App.js
@@ -7,58 +7,55 @@ const App = () => {
   const [data, setData] = useState({ array: [], uselessValue: null });
 
   //input Modify function
-  const onChange = useCallback(
-    e => {
-      const { name, value } = e.target;
+  const onChange = useCallback(e => {
+    const { name, value } = e.target;
 
-      setForm(
-        produce(form, draft => {
-          draft[name] = value;
-        })
-      );
-    },
-    [form]
-  );
-
-  //form add fucntion
-  const onSubmit = useCallback(e => {
-    e.preventDefault();
-    const info = {
-      id: nextId.current,
-      name: form.name,
-      username: form.username
-    };
-
-    setData(
-      produce(data, drafy => {
-        drafy.array.push(info);
+    setForm(
+      produce(draft => {
+        draft[name] = value;
       })
     );
+  }, []);
 
-    //form reset
-    setForm({
-      name: "",
-      username: ""
-    });
-    nextId.current += 1;
-  });
+  //form add fucntion
+  const onSubmit = useCallback(
+    e => {
+      e.preventDefault();
+      const info = {
+        id: nextId.current,
+        name: form.name,
+        username: form.username
+      };
 
-  //item remove function
-  const onRemove = useCallback(
-    id => {
       setData(
-        //   immer를 사용한다고 해서 꼭 코드가 깔끔해지는 것은 아니다.
-        //   상황에 맞게 코드가 복잡해 질 경우 immer을 사용하는 것으로 충분하다.
-        produce(data, draft => {
-          draft.array.splice(
-            draft.array.findIndex(info => info.id === 1),
-            1
-          );
+        produce(drafy => {
+          drafy.array.push(info);
         })
       );
+
+      //form reset
+      setForm({
+        name: "",
+        username: ""
+      });
+      nextId.current += 1;
     },
-    [data]
+    [form.name, form.username]
   );
+
+  //item remove function
+  const onRemove = useCallback(id => {
+    setData(
+      //   immer를 사용한다고 해서 꼭 코드가 깔끔해지는 것은 아니다.
+      //   상황에 맞게 코드가 복잡해 질 경우 immer을 사용하는 것으로 충분하다.
+      produce(draft => {
+        draft.array.splice(
+          draft.array.findIndex(info => info.id === 1),
+          1
+        );
+      })
+    );
+  }, []);
 
   return (
     <div>
